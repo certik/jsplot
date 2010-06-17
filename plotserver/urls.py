f10c94fea99f1087b8b57bac623e7bd805ea052e
Ondrej Certik <ondrej@certik.cz>
2010-06-16 17:30:41 -0700

    Serve raphael-min.js locally

--- plotserver/urls.py
+++ plotserver/urls.py
@@ -4,15 +4,11 @@ from django.conf.urls.defaults import *
 # from django.contrib import admin
 # admin.autodiscover()
 
+import os.path
+p = os.path.join(os.path.dirname(__file__), 'media_files/')
+
 urlpatterns = patterns('',
         (r'^$', 'plotserver.app.views.index'),
-    # Example:
-    # (r'^plotserver/', include('plotserver.foo.urls')),
-
-    # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
-    # to INSTALLED_APPS to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # (r'^admin/', include(admin.site.urls)),
+        (r'^media_files/(?P<path>.*)$', 'django.views.static.serve',
+                            {'document_root': p}),
 )
